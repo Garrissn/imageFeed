195053cee697a4ae847bc9df2fb70e37dccca9ec
Igor Polunin <garry.818@yandex.ru>
2023-03-20 21:43:16 +0300

    проверка комита последняя

--- imageFeed/AppDelegate.swift
+++ imageFeed/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        print("Heelo00000009890iooi source tree")
+        print("Heelo source tree")
         return true
     }
 
